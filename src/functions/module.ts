e5cd3e8fd344fa72f9c1aa90801a9981219fbfc7
ZachSand <zachsand@utexas.edu>
2021-12-21 00:11:20 -0600

    Updates for deployment and removing things not needed

--- src/functions/module.ts
+++
@@ -1,8 +0,0 @@
-import { ModuleInterface, ModuleResponseInterface } from "../types/module.js";
-
-export const m = async ({ text }: ModuleInterface): Promise<ModuleResponseInterface> => {
-	const app = () => text;
-	return {
-		app,
-	};
-};
--- src/tests/module.test.ts
+++
@@ -1,6 +0,0 @@
-import { m } from "../functions/module.js";
-
-test("show hello world", async () => {
-	const { app } = await m({ text: "hello-world" });
-	expect(app()).toBe("hello-world");
-});
